6a94da166500ee92c901fbf73d10f2fd2889842b
新城安基 <mittel@shinshiroyasumotonoMacBook-Pro.local>
2020-03-09 17:17:17 +0900

    ログイン前の画面遷移分まで完成。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'homes/top'
+  get 'homes/about'
+
+  root to: "homes#top"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_060513) do
+ActiveRecord::Schema.define(version: 2020_03_07_031045) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
